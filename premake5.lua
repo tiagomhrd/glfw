0c939246aab658063754cc22859c60b98b73384e
Tiago <tiagomoherdaui@gmail.com>
2022-02-25 18:28:23 -0300

    Premake update

--- premake5.lua
+++ premake5.lua
@@ -11,11 +11,16 @@ project "GLFW"
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
 		"src/context.c",
+		"src/platform.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/window.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/null_joystick.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
 	}
 	filter "system:linux"
 		pic "On"
@@ -50,6 +55,7 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
