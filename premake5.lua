ca24576cb8406928bcb6d4676a651f17d5b9a2e8
Tiago Moherdaui <tiagomoherdaui@gmail.com>
2024-08-08 17:26:19 -0300

    updated version and premake

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,23 @@ project "GLFW"
 		"src/monitor.c",
 		"src/window.c",
 		"src/vulkan.c",
+
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 	filter "system:linux"
 		pic "On"
@@ -49,14 +66,17 @@ project "GLFW"
 		files
 		{
 			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+		        "src/win32_module.c",
+		        "src/win32_joystick.c",
+		        "src/win32_monitor.c",
+		        "src/win32_time.h",
+		        "src/win32_time.c",
+		        "src/win32_thread.h",
+		        "src/win32_thread.c",
+		        "src/win32_window.c",
+		        "src/wgl_context.c",
+		        "src/egl_context.c",
+		        "src/osmesa_context.c"
 		}
 
 		defines 
